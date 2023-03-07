3d8ea7e63eb2470de0a436039889382e63cc12bb
LeeHyunSoo <fkdltj1@gmail.com>
2023-03-07 11:05:34 +0900

    백엔드dev 기본

---
+++ server/src/test/java/Main/mbti/MbtiApplicationTests.java
@@ -0,0 +1,13 @@
+package Main.mbti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
