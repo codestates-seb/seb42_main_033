31e77ee3bd6106cead68f87005c45fc96c41bb78
ByeongminBRO <abm40172138@gmail.com>
2023-03-27 19:41:19 +0900

    내 게시글완, 댓글x

--- client/src/page/MyCommentsPage.js
+++ client/src/page/MyCommentsPage.js
@@ -1,10 +1,131 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import MyPageSidebar from '../components/MypageSidebar.jsx';
-/*123*/
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 /*123*/
+
+// const comments = [
+//   {
+//     id: 1,
+//     text: '안녕',
+//   },
+//   {
+//     id: 2,
+//     text: '하세요',
+//   },
+//   {
+//     id: 3,
+//     text: '오오오오오',
+//   },
+//   {
+//     id: 4,
+//     text: '오오오오오',
+//   },
+//   {
+//     id: 5,
+//     text: '오오오오오',
+//   },
+//   {
+//     id: 6,
+//     text: '오오오오오',
+//   },
+//   {
+//     id: 7,
+//     text: '오오오오오',
+//   },
+//   {
+//     id: 8,
+//     text: '오오오오오',
+//   },
+// ];
+
+function MyComments() {
+  const [comments, setComments] = useState([]);
+  const [selectedComments, setSelectedComments] = useState([]);
+  const userId = localStorage.getItem('userId');
+  const token = localStorage.getItem('jwtToken');
+
+  const getComments = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/board/integrated/${userId}/comment/`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const userComments = response.data.filter(
+        (comment) => comment.userId === parseInt(userId)
+      );
+      const sortedUserComments = userComments.sort((a, b) => b.id - a.id);
+      return sortedUserComments;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getComments();
+  }, []);
+
+  const deleteComment = async () => {
+    try {
+      const deletePromises = selectedComments.map((commentId) => {
+        return axios.delete(`${URL}/board/integrated/1/comment/${commentId}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+      });
+      await Promise.all(deletePromises);
+      setComments(
+        comments.filter(
+          (comment) => !selectedComments.includes(comment.content)
+        )
+      );
+      setSelectedComments([]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleCheckboxClick = (id) => {
+    if (selectedComments.includes(id)) {
+      setSelectedComments(selectedComments.filter((cid) => cid !== id));
+    } else {
+      setSelectedComments([...selectedComments, id]);
+    }
+  };
+
+  return (
+    <>
+      <MyPageSidebar />
+      <CommentPageContainer>
+        <StyledText>내 댓글</StyledText>
+        <CommentsContainer>
+          <HeaderContainer>선택</HeaderContainer>
+          <hr />
+          {comments &&
+            comments.map((commentItem) => (
+              <CommentContainer key={commentItem.id}>
+                <input
+                  type="checkbox"
+                  id={`checkbox-${commentItem.id}`}
+                  checked={selectedComments.includes(commentItem.id)}
+                  onChange={() => handleCheckboxClick(commentItem.id)}
+                />
+                <CommentText htmlFor={`checkbox-${commentItem.id}`}>
+                  <p>{commentItem.content}</p>
+                </CommentText>
+              </CommentContainer>
+            ))}
+        </CommentsContainer>
+        <DeleteButton onClick={deleteComment}> 삭제 </DeleteButton>
+      </CommentPageContainer>
+    </>
+  );
+}
 const CommentPageContainer = styled.div`
   font-size: 1vw;
   display: flex;
@@ -68,128 +189,4 @@ const CommentText = styled.label`
   margin-left: 5vw;
   margin-top: 0px;
 `;
-
-// const comments = [
-//   {
-//     id: 1,
-//     text: '안녕',
-//   },
-//   {
-//     id: 2,
-//     text: '하세요',
-//   },
-//   {
-//     id: 3,
-//     text: '오오오오오',
-//   },
-//   {
-//     id: 4,
-//     text: '오오오오오',
-//   },
-//   {
-//     id: 5,
-//     text: '오오오오오',
-//   },
-//   {
-//     id: 6,
-//     text: '오오오오오',
-//   },
-//   {
-//     id: 7,
-//     text: '오오오오오',
-//   },
-//   {
-//     id: 8,
-//     text: '오오오오오',
-//   },
-// ];
-
-function MyComments() {
-  const [comments, setComments] = useState([]);
-  const [selectedComments, setSelectedComments] = useState([]);
-
-  const userId = useParams();
-  const token = localStorage.getItem('access_token');
-
-  const URL = `https://5293-211-217-72-99.jp.ngrok.io`;
-  const getComments = async () => {
-    try {
-      const response = await axios.get(
-        `${URL}/board/integrated/1/comment/${userId.id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      console.log(response);
-      setComments(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  useEffect(() => {
-    getComments();
-  }, []);
-
-  const deleteComment = async () => {
-    try {
-      const deletePromises = selectedComments.map((commentId) => {
-        return axios.delete(`${URL}/board/integrated/1/comment/${commentId}`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-      });
-      await Promise.all(deletePromises);
-      setComments(
-        comments.filter(
-          (comment) => !selectedComments.includes(comment.content)
-        )
-      );
-      setSelectedComments([]);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleCheckboxClick = (id) => {
-    if (selectedComments.includes(id)) {
-      setSelectedComments(selectedComments.filter((cid) => cid !== id));
-    } else {
-      setSelectedComments([...selectedComments, id]);
-    }
-  };
-
-  return (
-    <>
-      <MyPageSidebar />
-      <CommentPageContainer>
-        <StyledText>내 댓글</StyledText>
-        <CommentsContainer>
-          <HeaderContainer>선택</HeaderContainer>
-          <hr />
-          {comments
-            .slice()
-            .reverse()
-            .map((comment, index) => (
-              <CommentContainer key={index}>
-                <input
-                  type="checkbox"
-                  id={`checkbox-${index}`}
-                  checked={selectedComments.includes(index)}
-                  onChange={() => handleCheckboxClick(index)}
-                />
-                <CommentText htmlFor={`checkbox-${index}`}>
-                  <p>{comment.content}</p>
-                </CommentText>
-              </CommentContainer>
-            ))}
-        </CommentsContainer>
-        <DeleteButton onClick={deleteComment}> 삭제 </DeleteButton>
-      </CommentPageContainer>
-    </>
-  );
-}
-
 export default MyComments;
